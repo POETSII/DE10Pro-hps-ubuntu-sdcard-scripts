95f0bc7aa0bb7e70763e3f725c6182b041363030
Theo Markettos <atm26@cl.cam.ac.uk>
2019-04-03 14:24:43 +0100

    Implement support for installing Ubuntu packages via a chroot and qemu-arm

--- ubuntu_packages.sh
+++ ubuntu_packages.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -e
+#!/bin/sh
 #-
 # SPDX-License-Identifier: BSD-2-Clause
 #
-# Copyright (c) 2018 A. Theodore Markettos
+# Copyright (c) 2019 A. Theodore Markettos
 # All rights reserved.
 #
 # This software was developed by SRI International and the University of
@@ -32,83 +32,22 @@
 # SUCH DAMAGE.
 #
 
-
-FPGA_DIR=$1
-FPGA_PROJECT=$2
-QSYS=$3
-PAYLOAD=$4
-FPGA_HANDOFF_DIR=hps_isw_handoff
-FPGA_BITFILE_RBF=$FPGA_DIR/output_files/$FPGA_PROJECT.rbf
-SD_IMAGE=sdimage.img
-ROOT_SIZE_MB=1500
-SD_SIZE_MB=2048
-echo $SCRIPT_PATH
+INSTALL=$1
 shift
-shift
-shift
-shift
-# parameters 5 and later
 PACKAGES="$@"
 
-DTB=socfpga_arria10_socdk_sdmmc.dtb
+# make sure we have a qemu binary
+sudo apt-get install -y qemu-user-static
 
-SCRIPT_NAME=$(readlink -f "$0")
-SCRIPT_PATH=$(dirname "$SCRIPT_NAME")
+# put it inside the chroot
+sudo cp /usr/bin/qemu-arm-static $INSTALL/tmp/qemu-arm-static
 
-function ubuntu() {
-	$SCRIPT_PATH/fetch_ubuntu.sh
-	$SCRIPT_PATH/configure_system.sh mnt/2/
-	$SCRIPT_PATH/configure_networking.sh mnt/2/
-	$SCRIPT_PATH/ubuntu_packages.sh mnt/2/ $PACKAGES
-	if [ -n "$PAYLOAD" ] ; then
-		echo "Copying extra files into tree"
-		cp -av $PAYLOAD/* mnt/2/
-	fi
-}
+# ensure DNS is functional
+sudo mkdir -p $INSTALL/run/resolvconf
+echo "nameserver 8.8.8.8" | sudo tee $INSTALL/run/resolvconf/resolv.conf
 
+sudo chroot $INSTALL /tmp/qemu-arm-static /usr/bin/apt-get -y update
+sudo chroot $INSTALL /tmp/qemu-arm-static /usr/bin/apt-get -y upgrade
+sudo chroot $INSTALL /tmp/qemu-arm-static /usr/bin/apt-get -y install $PACKAGES
 
-function kernel() {
-	$SCRIPT_PATH/build_linux.sh
-}
-
-function uboot() {
-	$SCRIPT_PATH/make_uboot.sh $FPGA_DIR/$FPGA_HANDOFF_DIR
-	cp -a bsp/uboot_w_dtb-mkpimage.bin .
-}
-
-function devicetree() {
-	$SCRIPT_PATH/make_device_tree.sh $FPGA_DIR $QSYS.sopcinfo
-
-	cp -a $FPGA_DIR/$DTB $DTB	
-}
-
-function bitfile() {
-#	$SCRIPT_PATH/make_bitfile.sh $FPGA_DIR $FPGA_PROJECT
-	cp -a $FPGA_BITFILE_RBF socfpga.rbf
-}
-
-function sdimage() {
-
-	echo "Building SD card image"
-	sudo $SCRIPT_PATH/make_sdimage.py -f	\
-		-P uboot_w_dtb-mkpimage.bin,num=3,format=raw,size=10M,type=A2 \
-		-P mnt/2/*,num=2,format=ext3,size=${ROOT_SIZE_MB}M \
-		-P zImage,socfpga.rbf,socfpga_arria10_socdk_sdmmc.dtb,num=1,format=vfat,size=500M \
-		-s ${SD_SIZE_MB}M \
-		-n $SD_IMAGE
-
-}
-
-
-function tidy() {
-	sudo umount mnt/1 mnt/2
-}
-
-
-ubuntu
-kernel
-uboot
-devicetree
-bitfile
-sdimage
-tidy
+sudo rm $INSTALL/run/resolvconf/resolv.conf
